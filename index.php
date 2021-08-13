b822bc676483dbd9c39c74340fbacd0449caae04
graveeler2 <57375301+netocastro@users.noreply.github.com>
2021-08-13 01:29:45 -0300

    limpando index
    Linmpando arquivo index .php, pois estava cheio de comentarios de debugs.

--- index.php
+++ index.php
@@ -5,55 +5,11 @@ use Stonks\Router\Router;
 
 require __DIR__ ."/vendor/autoload.php";  
 
-
-
-
 $router = New Router(BASE_PATH);
 
 $router->namespace('Source\Controllers');
 
 $router->group(null);
-
-$router->get('/',function ()
-{
-
-      
-    /*  $data = [];
-      $data['nome'] = "hfjhjfdf";
-      $data['crm'] = "88";
-      $data['id'] = "10";
-
-
-      foreach ($data as $key => $value) {
-            echo "key: " . $key . "<br>";
-            echo "value: " . $value. "<br><br>";
-            echo 'datakey:' . $data[$key]. "<br>";
-
-      }*/
-
-     /* var_dump((new Medicos())->findById(5)->data());
-     $objectModel = (new Medicos())->findById(5);
-      foreach ($objectModel->data() as $key => $value) {
-            echo "key: " . $key . "<br>";
-            echo "value: " . $value. "<br>";
-            if(isset($data[$key])){
-                  $objectModel->$key = $data[$key];
-            }
-      }
-      $objectModel->change()->save();
-
-      if($objectModel->fail()){
-            echo json_encode($objectModel->fail()->getMessage());
-      }else{
-            echo json_encode("salvo");
-      }
-
-      echo "<br><br><br>";
-
-      var_dump((new Medicos())->findById(5)->data());*/
-
-
-});
 
 /**
  * GET
@@ -64,8 +20,7 @@ $router->get('/{$model}','Get:getTables','get.tables');
 /**
  * POST
  */
-//$router->post('/pacientes','Post:postPaciente','post.paciente');
-//$router->post('/medicos','Post:postMedico','post.medico');
+
 $router->post('/{$model}','Post:inserir','post.insert');
 
 /**
